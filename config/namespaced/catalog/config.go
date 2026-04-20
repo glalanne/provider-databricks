f9ef5f71f32179383bb128e52c96e107fdd041ca
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-04-20 19:26:31 +0000

    fix: databricks_catalog constantly reconcile loop

--- config/namespaced/catalog/config.go
+++ config/namespaced/catalog/config.go
@@ -6,5 +6,6 @@ import "github.com/crossplane/upjet/v2/pkg/config"
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("databricks_catalog", func(r *config.Resource) {
 		r.ShortGroup = "unity"
+		r.SchemaElementOptions.SetAddToObservation("provisioning_info")
 	})
 }
