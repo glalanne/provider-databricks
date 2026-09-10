40990526b44808e8257c4410bad9deb296632aa9
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-09-10 12:27:23 -0700

    Update config.go

--- config/namespaced/job/config.go
+++ config/namespaced/job/config.go
@@ -15,7 +15,6 @@ func Configure(p *config.Provider) {
 			"run_job_task", "git_source", "email_notifications", "webhook_notifications",
 			"notification_settings", "library", "parameter", "environment", "tags",
 		)
-		common.ClearStaleBlocksBeforeRead(r, "provider_config")
 
 		r.References["notebook_task.warehouse_id"] = config.Reference{
 			TerraformName: "databricks_sql_endpoint",
