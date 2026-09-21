8b452084f4252b07105b9d337d45e149006bc67a
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-09-21 21:52:17 +0000

    fix(Catalog): move provisioningInfo to Status

--- apis/cluster/unity/v1alpha1/zz_generated.deepcopy.go
+++ apis/cluster/unity/v1alpha1/zz_generated.deepcopy.go
@@ -954,13 +954,6 @@ func (in *CatalogInitParameters) DeepCopyInto(out *CatalogInitParameters) {
 		*out = new(string)
 		**out = **in
 	}
-	if in.ProvisioningInfo != nil {
-		in, out := &in.ProvisioningInfo, &out.ProvisioningInfo
-		*out = make([]ProvisioningInfoInitParameters, len(*in))
-		for i := range *in {
-			(*in)[i].DeepCopyInto(&(*out)[i])
-		}
-	}
 	if in.ShareName != nil {
 		in, out := &in.ShareName, &out.ShareName
 		*out = new(string)
@@ -1310,13 +1303,6 @@ func (in *CatalogParameters) DeepCopyInto(out *CatalogParameters) {
 		in, out := &in.ProviderName, &out.ProviderName
 		*out = new(string)
 		**out = **in
-	}
-	if in.ProvisioningInfo != nil {
-		in, out := &in.ProvisioningInfo, &out.ProvisioningInfo
-		*out = make([]ProvisioningInfoParameters, len(*in))
-		for i := range *in {
-			(*in)[i].DeepCopyInto(&(*out)[i])
-		}
 	}
 	if in.ShareName != nil {
 		in, out := &in.ShareName, &out.ShareName
