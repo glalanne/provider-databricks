64f908273109b25525bece608cad9bba1825f814
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-09-21 21:51:22 +0000

    fix(Pipeline): remove provisioning_info.# from Diff

--- config/namespaced/pipeline/config.go
+++ config/namespaced/pipeline/config.go
@@ -1,6 +1,11 @@
 package pipeline
 
-import "github.com/crossplane/upjet/v2/pkg/config"
+import (
+	"strings"
+
+	"github.com/crossplane/upjet/v2/pkg/config"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
@@ -19,6 +24,24 @@ func Configure(p *config.Provider) {
 		if s, ok := r.TerraformResource.Schema["latest_updates"]; ok {
 			s.Optional = false
 			s.Computed = true
+		}
+
+		r.TerraformCustomDiff = func(
+			diff *terraform.InstanceDiff,
+			state *terraform.InstanceState,
+			cfg *terraform.ResourceConfig,
+		) (*terraform.InstanceDiff, error) {
+			if diff == nil {
+				return diff, nil
+			}
+
+			for k := range diff.Attributes {
+				if strings.HasPrefix(k, "latest_updates.") {
+					delete(diff.Attributes, k)
+				}
+			}
+
+			return diff, nil
 		}
 	})
 }
